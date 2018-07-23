acf8a0bc131596a871f4d85db0df2d8c186977bc
Glauber <grobis_mn@hotmail.com>
2018-07-23 20:17:33 -0300

    etapa16

---
+++ ControleDeEstoque.Web/Helper/CriptoHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ControleDeEstoque.Web
+{
+    public  static class CriptoHelpers
+    {
+
+        //toda classe de criptografia trabalha com bits
+        public static string HashMd5(string val)
+        {
+            var bytes = Encoding.ASCII.GetBytes(val);
+            var Md5 = MD5.Create();
+            var hash = Md5.ComputeHash(bytes);
+            var retorno = string.Empty;
+            for(int i =0; i < hash.Length; i++)
+            {
+
+                //usando o x2 ele converte para hexadecimal, para ser a propria representação do MD5
+                retorno += hash[i].ToString("x2");
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ testes/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testes.Models;
+
+namespace testes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizarData()
+        {
+            var listaData = TesteData.RecuperarLista();
+
+            return View(listaData);
+        }
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
